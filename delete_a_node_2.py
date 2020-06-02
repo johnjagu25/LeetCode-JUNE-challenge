0506fafa18df91aaafcc545588a14bd41ce756ef
unknown <johnjagu25@gmail.com>
2020-06-02 21:05:20 +0530

    day 2 challenge

---
+++ delete_a_node_2.py
@@ -0,0 +1,14 @@
+# The linked list will have at least two elements.
+# All of the nodes' values will be unique.
+# The given node will not be the tail and it will always be a valid node of the linked list.
+# Do not return anything from your function.
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def deleteNode(self, node):
+        node.val = node.next.val
+        node.next = node.next.next

974689bfa84054bb746ed9eb28fecca422363d71
unknown <johnjagu25@gmail.com>
2020-06-14 12:47:36 +0530

    day 7 challenge

--- coin_comb_7.py
+++ coin_comb_7.py
@@ -7,17 +7,12 @@
 # 5=1+1+1+1+1
 
 def coinComb(arr,m,n):
-    dp = [[0 for i in range(m)] for j in range(n+1)]
-    for i in range(m):
-        dp[0][i] = 1
-    for i in range(1,n+1):
-        for j in range(0,m):
-            # excluding coin
-            x =  dp[i][j-1] if j >= 0 else 0
-            # including coin
-            y =  dp[i-arr[j]][j] if i >= arr[j] else 0
-            dp[i][j] = x + y
-    return (dp[n][m-1])
+    dp = [0] * (n+1)
+    dp[0] = 1
+    for coin in arr:
+        for x in range(coin,n+1):
+            dp[x] += dp[x-coin]
+    return dp[n]
 arr = [1,2,3]
 m = len(arr)    
 print(coinComb(arr,m,4))
